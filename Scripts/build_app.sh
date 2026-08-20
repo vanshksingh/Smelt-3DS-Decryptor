60b2cc51f27f03021d3b9716b770b79ab8bfc0b9
vks <vsvsasas@gmail.com>
2026-08-20 18:33:04 +0530

    chore: bump version to v1.0.2 and update build bundle configuration

--- Scripts/build_app.sh
+++ Scripts/build_app.sh
@@ -81,9 +81,9 @@ cat << 'EOF' > "${BUNDLE_DIR}/Contents/Info.plist"
     <key>CFBundlePackageType</key>
     <string>APPL</string>
     <key>CFBundleShortVersionString</key>
-    <string>1.0.1</string>
+    <string>1.0.2</string>
     <key>CFBundleVersion</key>
-    <string>2</string>
+    <string>3</string>
     <key>LSMinimumSystemVersion</key>
     <string>11.0</string>
     <key>NSHighResolutionCapable</key>
